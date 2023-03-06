ce9bdfaf76eb8130bff562f4f55440c848dd999f
BrayanCJR7 <brayancamilo39@gmail.com>
2023-03-06 15:59:40 -0500

    First commit

--- src/Pages/Bata-bit/components/moneda-bata.jsx
+++ src/Pages/Bata-bit/components/moneda-bata.jsx
@@ -1,5 +1,17 @@
 import '../bata-bit.css'
 
+const monedas = [
+    {nameCoin: 'Bitcoin', priceCoin: 1.96, stonks: false},
+    {nameCoin: 'Ethereum', priceCoin: 0.07, stonks: true},
+    {nameCoin: 'Ripple', priceCoin: 2.15, stonks: false},
+    {nameCoin: 'Stellar', priceCoin: 4.96, stonks: false}
+]
+const comisiones = [
+    {nameCommissions: 'Bitrade', priceCommissions: 12.96},
+    {nameCommissions: 'Bitpreco', priceCommissions: 13.07},
+    {nameCommissions: 'Novadax', priceCommissions: 13.15},
+    {nameCommissions: 'Coinext', priceCommissions: 14.96}
+]
 export const MonedaBata = () => {
     return (
         <>
@@ -11,8 +23,11 @@ export const MonedaBata = () => {
                 <p className="BitCoin-text">
                     Traemos información en tiempo real de las casas de cambio y las monedas más importantes del mundo.
                 </p>
-                <a href="Bata-bit/components" className="btn btnMonedas">Valores en <b>$ USD</b></a>
-                <a href="Bata-bit/components" className="btn btnComisiones">Valores en <b>$ USD</b></a>
+                <div className="containerInfoTasas">
+                    <p className="titleTasas">Monedas</p>
+                </div>
+                <a href="#" className="btn btnMonedas"><b>Actualizado:</b> 19 Julio 23:45</a>
+                {/*<a href="Bata-bit/components" className="btn btnComisiones">Valores en <b>$ USD</b></a>*/}
             </section>
         </>
     )
--- src/home.jsx
+++ src/home.jsx
@@ -11,13 +11,14 @@ const Home = () => {
                 <Nav/>
                 <TabPages/>
                 <ReusableTabPages tabNames={["contenido dinamico", "tab2", "tab-3"]}>
+                    <div>
+                        <Tags/>
+                    </div>
                     <div>Cambio de pestañas con un componente reutilizable</div>
                     <div>Se pueden agregar los botones que se quieran, pero se deben agregar la misma cantidad de
                         "div"
                     </div>
-                    <div></div>
                 </ReusableTabPages>
-                <Tags/>
             </section>
         </>
     )
