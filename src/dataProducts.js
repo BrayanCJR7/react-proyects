1756a1aefc8d5ce679838e8f671ddc8b0d3a6186
brayancjr7 <bjimenez@threan.com>
2023-03-13 18:45:46 -0500

    data products

---
+++ src/dataProducts.js
@@ -0,0 +1,468 @@
+export const Products = [
+    {
+        "pro_codigo": "BIP610108",
+        "pro_nombre": "EURICAN DAP-L MHPL MULTI 10 DOSIS JERINGAS SIN CARGO",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": true,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP610108.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 630,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,antibiotico,estupefaciente,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": false
+    },
+    {
+        "pro_codigo": "BIP000003",
+        "pro_nombre": "NECESER FL VACIO PETCARE",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": true,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP000003.jpeg",
+        "pro_laboratorio": "BOEHRINGER INGELHEIM",
+        "pro_precio": 190,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "false,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": false
+    },
+    {
+        "pro_codigo": "BIP161301",
+        "pro_nombre": "PREVICOX 57 MG 10 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": true,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161301.jpeg",
+        "pro_laboratorio": "MERCK SHARP&DOHME ANIMAL HEALTH,S.L.",
+        "pro_precio": 725880,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": true,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP161302",
+        "pro_nombre": "PREVICOX 57 MG 30 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": true,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161302.jpeg",
+        "pro_laboratorio": "ECOMPUTER",
+        "pro_precio": "...",
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": false
+    },
+    {
+        "pro_codigo": "BIP161476",
+        "pro_nombre": "PREVICOX 227 MG 180 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": true,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161476.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": "...",
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP540591",
+        "pro_nombre": "NEXGARD PERRO 2-4 KG 3 COMP",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP540591.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 45.5,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "NUTRICION",
+                "macrofamilia_cod": "2",
+                "Familia": [
+                    {
+                        "familia": "Nutrición y Alimentación",
+                        "familia_cod": "2.1"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP561224",
+        "pro_nombre": "STOMORGYL 2 KG 50 COMPRIMIDOS E.C.",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP561224.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 15,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "false,antibiotico,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP600998",
+        "pro_nombre": "EURICAN DAP-LR MHPLR 10 DOSIS JERINGAS SIN CARGO",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP600998.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 750,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": false
+    },
+    {
+        "pro_codigo": "BIPBIP",
+        "pro_nombre": "CANITENOL 987987",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIPBIP.jpeg",
+        "pro_laboratorio": "BOEHRINGER INGELHEIM",
+        "pro_precio": 5,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": true,
+        "pro_caracteristicas": "false,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "HIGIENE",
+                "macrofamilia_cod": "3",
+                "Familia": [
+                    {
+                        "familia": "Biocidas y Plagicidas",
+                        "familia_cod": "3.2"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BLAS00001",
+        "pro_nombre": "ATOPICA 100 mg CAPSULAS BLANDAS PARA PERROS",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BLAS00001.jpeg",
+        "pro_laboratorio": "BOEHRINGER INGELHEIM",
+        "pro_precio": 78,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP161298",
+        "pro_nombre": "PREVICOX 57 MG 180 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161298.jpeg",
+        "pro_laboratorio": "BOEHRINGER INGELHEIM",
+        "pro_precio": 145,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": true,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Antiparasitarios",
+                        "familia_cod": "1.2"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP161301",
+        "pro_nombre": "PREVICOX 57 MG 10 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161301.jpeg",
+        "pro_laboratorio": "MERCK SHARP&DOHME ANIMAL HEALTH,S.L.",
+        "pro_precio": 725880,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": true,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP161302",
+        "pro_nombre": "PREVICOX 57 MG 30 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161302.jpeg",
+        "pro_laboratorio": "ECOMPUTER",
+        "pro_precio": "...",
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": false
+    },
+    {
+        "pro_codigo": "BIP161476",
+        "pro_nombre": "PREVICOX 227 MG 180 COMPRIMIDOS DIVISIBLES EN 4",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP161476.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": "...",
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP540591",
+        "pro_nombre": "NEXGARD PERRO 2-4 KG 3 COMP",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP540591.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 45.5,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "NUTRICION",
+                "macrofamilia_cod": "2",
+                "Familia": [
+                    {
+                        "familia": "Nutrición y Alimentación",
+                        "familia_cod": "2.1"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP561224",
+        "pro_nombre": "STOMORGYL 2 KG 50 COMPRIMIDOS E.C.",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP561224.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 15,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "false,antibiotico,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BIP600998",
+        "pro_nombre": "EURICAN DAP-LR MHPLR 10 DOSIS JERINGAS SIN CARGO",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIP600998.jpeg",
+        "pro_laboratorio": "",
+        "pro_precio": 750,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": false
+    },
+    {
+        "pro_codigo": "BIPBIP",
+        "pro_nombre": "CANITENOL 987987",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BIPBIP.jpeg",
+        "pro_laboratorio": "BOEHRINGER INGELHEIM",
+        "pro_precio": 5,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": true,
+        "pro_caracteristicas": "false,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "HIGIENE",
+                "macrofamilia_cod": "3",
+                "Familia": [
+                    {
+                        "familia": "Biocidas y Plagicidas",
+                        "familia_cod": "3.2"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "pro_codigo": "BLAS00001",
+        "pro_nombre": "ATOPICA 100 mg CAPSULAS BLANDAS PARA PERROS",
+        "pro_unidades_disponibles": "Consultar disponibilidad",
+        "pro_nuevo": false,
+        "pro_destacado": false,
+        "pro_mas_vendido": false,
+        "pro_imagen": "http://localhost:8003/product/BLAS00001.jpeg",
+        "pro_laboratorio": "BOEHRINGER INGELHEIM",
+        "pro_precio": 78,
+        "pro_especies": "",
+        "pro_carfrio": false,
+        "pro_favorito": false,
+        "pro_caracteristicas": "prescripcion,false,false,false,false,",
+        "pro_fecha": "2023-02-28",
+        "pro_categorias": [
+            {
+                "macrofamilia": "MEDICAMENTOS",
+                "macrofamilia_cod": "1",
+                "Familia": [
+                    {
+                        "familia": "Farmacológicos ",
+                        "familia_cod": "1.3"
+                    }
+                ]
+            }
+        ]
+    }
+]
